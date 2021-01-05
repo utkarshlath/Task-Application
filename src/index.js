96a8808ec6622c8349ec2ee333f9685ff3dc564e
Utkarsh Lath <utkarshlath2@gmail.com>
2021-01-05 22:34:06 +0530

    Add support for file upload

--- src/index.js
+++ src/index.js
@@ -6,16 +6,7 @@ const taskRouter = require('./routers/tasks')
 const app = express()
 const port = process.env.PORT || 3000
 
-// app.use((req,res,next)=>{
-//     if(req.method==="GET"){
-//         res.send('GET requests are disabled!')
-//     }else{
-//         next()
-//     }
-// })
-// app.use((req,res,next)=>{
-//     res.status(503).send('Site under maintenance!')
-// })
+
 
 app.use(express.json())
 app.use(userRouter)
--- src/routers/users.js
+++ src/routers/users.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
 const User = require('../models/users')
 const auth = require('../middleware/auth')
 const router = new express.Router()
@@ -74,6 +76,51 @@ router.delete('/users/me', auth, async (req, res)=>{
         res.send(req.user)
     }catch (e){
         res.status(500).send(e)
+    }
+})
+
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb){
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+            cb(new Error('Please upload an image!'))
+        }
+        cb(undefined,true)
+    }
+})
+
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req,res)=>{
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
+},(error, req, res, next)=>{
+    res.status(400).send({error: error.message})
+})
+
+router.delete('/users/me/avatar', auth, async (req, res)=>{
+    try{
+        req.user.avatar = undefined
+        await req.user.save()
+        res.send()
+    }catch (e){
+        res.status(500).send(e)
+    }
+})
+
+router.get('/users/:id/avatar', async (req, res)=>{
+    try{
+        const user = await User.findById(req.params.id)
+
+        if(!user || !user.avatar){
+            throw new Error()
+        }
+        res.set('Content-Type','image/png')
+        res.send(user.avatar)
+    }catch(e){
+        res.status(404).send()
     }
 })
 
